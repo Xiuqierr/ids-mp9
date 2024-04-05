ec955cfb59035212e15bc280b2f9f7da78cba49c
Xiuqi CHEN <x.chen@duke.edu>
2024-04-05 18:09:14 -0400

    Update

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from transformers import pipeline
 st.title('My AI Chatbot')
 user_input = st.text_input("Talk to the chatbot:")
 
-chat = pipeline('text-generation', model='microsoft/DialoGPT-medium')
+model = pipeline("text-generation", model="openai-gpt")
 
 if user_input:
-    response = chat(user_input)[0]['generated_text']
+    response = model(user_input)[0]['generated_text']
     st.text_area("Chatbot says:", value=response, height=100, max_chars=None, key=None)
